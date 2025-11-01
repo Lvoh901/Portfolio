85dc50bde008d8fea0884d898caabeb4bb5810a2
Lvoh901 <ochiengelvis90@gmail.com>
2025-11-01 20:17:54 +0300

    supabase connection

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://dzrijcqmcylpgihfeziw.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cmlqY3FtY3lscGdpaGZleml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5ODAxNzAsImV4cCI6MjA3NzU1NjE3MH0.f4WhKdxus8Bp8n6RxPCiJ7alVelWMTOgyFnR6kbc0pw"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
